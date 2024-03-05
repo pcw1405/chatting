4991a2081e62523fa832106c9b9976498e361688
chungwon <pcw14057@gmail.com>
2024-03-06 04:35:40 +0900

    채팅 메시지 구현

---
+++ src/main/java/com/example/chatting/Dto/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.example.chatting.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType{
+        ENTER,TALK
+    }
+    private MessageType type;
+    private String roomId; //방번호
+    private String sender; // 메시지 보낸 사람
+    private String message; // 메시지
+
+}
