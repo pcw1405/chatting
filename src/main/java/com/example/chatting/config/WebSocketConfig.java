ae314344a2f59f398e2eda1016f7fe3d43aeacbf
chungwon <pcw14057@gmail.com>
2024-03-10 03:32:56 +0900

    WebSocketConfig stomp에 맞게 수정

--- src/main/java/com/example/chatting/config/WebSocketConfig.java
+++ src/main/java/com/example/chatting/config/WebSocketConfig.java
@@ -3,22 +3,38 @@ package com.example.chatting.config;
 import com.example.chatting.handler.WebSocketChatHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketChatHandler chatHandler;
 
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+    // 메시지를 발행하는 요청의 prefix는 /pub로 시작하도록 설정하고 메시지를
+    // 구독하는 요청의 prefix는 /sub로 시작하도록 설정합니다
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
-        registry.addHandler(chatHandler,"ws/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+                .withSockJS();
+
     }
+    // endpoint를 /ws-stomp로 설정을 한다
+
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+//        registry.addHandler(chatHandler,"ws/chat").setAllowedOrigins("*");
+//    }
 //    도메인이 다른 서버에 접속할 수 있도록
 
 
