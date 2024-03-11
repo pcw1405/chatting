1e60313d3283228cbc109a62004d6dd23596386f
chungwon <pcw14057@gmail.com>
2024-03-12 08:31:58 +0900

    redis 임베디드 설정 maxheap관련 설정 해결 필요

--- src/main/java/com/example/chatting/dto/ChatRoom.java
+++ src/main/java/com/example/chatting/dto/ChatRoom.java
@@ -3,40 +3,25 @@ package com.example.chatting.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
-//    private Set<WebSocketSession> sessions =new HashSet<>();
-            // 클라이언트 정보를 가지고 있어야 한다
-            //WebsocketSession 정보 리스트를 멤버 필드로 갖는다
 
-//    @Builder
-    public static ChatRoom create(String name){
+    public static ChatRoom create(String name) {
         ChatRoom chatRoom = new ChatRoom();
-        chatRoom.roomId= UUID.randomUUID().toString();
-        chatRoom.name=name;
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
         return chatRoom;
     }
-// 웹소켓 관리가 필요없어지므로 세션관리가 필요없어진다 간소화
-
-
-    // handleActions를 통해 분기처릴르 한다
-//    public void handleActions(WebSocketSession session,  ChatMessage chatMessage,ChatService chatService){
-//        if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
-//            sessions.add(session);
-//            chatMessage.setMessage(chatMessage.getSender()+ "님이 입장하셨습니다");
-//
-//        }
-//        sendMessage(chatMessage,chatService);
-//    }
-//
-//    public <T> void sendMessage(T message,ChatService chatService){
-//        sessions.parallelStream().forEach(session -> chatService.sendMessage(session,message));
-//    }
-
-
 }
+
+
+
