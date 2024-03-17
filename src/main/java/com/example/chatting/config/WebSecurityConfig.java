8b9950672785b45cec604bc8d7522de3772f3249
chungwon <pcw14057@gmail.com>
2024-03-17 11:39:07 +0900

    시큐리티 추가중

---
+++ src/main/java/com/example/chatting/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.chatting.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .headers()
+                    .frameOptions().sameOrigin()
+                .and()
+                    .formLogin()
+                .and()
+                    .authorizeRequests()
+                        .antMatchers("/chat/**").hasRole("USER")
+                        .anyRequest().permitAll();
+    }
+    /** 테스트를 위해 In-memory에 계정을 임의로 생성한다.
+     **/
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                    .withUser("happydaddy")
+                    .password("{noop}1234")
+                    .roles("USER")
+                .and()
+                    .withUser("a")
+                    .password("{noop}1234")
+                    .roles("USER")
+                .and()
+                .withUser("a")
+                .password("{noop}1234")
+                .roles("GUEST");
+    }
+
+}
