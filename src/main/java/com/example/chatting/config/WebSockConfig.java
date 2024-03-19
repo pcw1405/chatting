38df826ed1e966e2b1ad22b21cb54bd9bd4020d2
chungwon <pcw14057@gmail.com>
2024-03-19 17:27:09 +0900

    오류가 생긴 이유는 private final StompHandler stompHandler; 에서 final이 없었기 때문

--- src/main/java/com/example/chatting/config/WebSockConfig.java
+++ src/main/java/com/example/chatting/config/WebSockConfig.java
@@ -23,8 +23,9 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
-                .withSockJS(); // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket이 동작할수 있게 합니다.
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOrigins("http://localhost:8081", "https://yourdomain.com")
+                .withSockJS();
     }
 
     @Override
